6cbeb0c391b7f6243ea9d39d80424435a4fe76b5
Nawat <69106196+nawat3329@users.noreply.github.com>
2022-05-01 15:19:05 +0700

    main page

--- main/weather.js
+++ main/weather.js
@@ -1,15 +1,31 @@
-function callAPI(){
+api_key = '813eeecd2f39020f2a5e609f5f1731f7';
+function findLocation(geolocation){
     $.ajax({
         type: 'GET',
         dataType: "json",
-        url: 'https://api.openweathermap.org/geo/1.0/direct?q=Bangkok&limit=1&appid=813eeecd2f39020f2a5e609f5f1731f7',
+        url: 'https://api.openweathermap.org/geo/1.0/direct',
+        data: {
+            q: geolocation,
+            limit : 1,
+            appid : api_key
+        },
         success: function (response) {
             console.log('response: ', response);
+            if(!$.trim(response)){
+                console.log("no location found");
+                alert("Unknown Location")
+            }
+            else{
+            getCurrentWeather(response[0].lat,response[0].lon);
+            }
         },
         error: function (response) {
             console.log('response: ', response);
+            return "error";
         },
     });
+    
+    
 
 }
 function getCurrentLocation(){
@@ -17,14 +33,80 @@ function getCurrentLocation(){
         //try to get user current location using getCurrentPosition() method
         navigator.geolocation.getCurrentPosition(function(position){ 
                 console.log("Found your location \nLat : "+position.coords.latitude+" \nLang :"+ position.coords.longitude);
+                getCurrentWeather(position.coords.latitude,position.coords.longitude);
             });
     }else{
         console.log("Browser doesn't support geolocation!");
     }
 }
 
+function getCurrentWeather(lat,lon){
+    $.ajax({
+        type: 'GET',
+        dataType: "json",
+        url: 'https://api.openweathermap.org/data/2.5/weather',
+        data: { 
+            lat: lat,
+            lon: lon,
+            units: "metric",
+            appid : api_key
+        },
+        success: function (response) {
+            console.log('response: ', response);
+            var card = '' + 
+            '<div class="col-md-8 col-lg-6 col-xl-4">' + 
+            '                                <div class="card" style="color: #4B515D; border-radius: 35px;">' + 
+            '                                    <div class="card-body p-4">' + 
+            '                                        <div class="d-flex">' + 
+            '                                            <h6 class="flex-grow-1">'+ response.name +'</h6><!-- city -->' + 
+            '                                            <h6>'+ new Date().toString("HH:mm")+'</h6><!-- time -->' + 
+            '                                        </div>' + 
+            '                                        <div class="d-flex flex-column text-center mt-5 mb-4">' + 
+            '                                            <h6 class="display-4 mb-0 font-weight-bold" style="color: #1C2331;"> '+ response.main.temp + '°C' + 
+            '                                            </h6>' + 
+            '                                            <span class="small" style="color: #868B94">'+response.weather[0].main+'</span><!-- weather -->' + 
+            '                                        </div>' + 
+            '                                        <div class="d-flex align-items-center">' + 
+            '                                            <div class="flex-grow-1" style="font-size: 1rem;">' + 
+            '                                                <div><i class="fas fa-wind fa-fw" style="color: #868B94;"></i> <span' + 
+            '                                                        class="ms-1">'+ response.wind.speed+' km/h' + 
+            '                                                    </span></div> <!-- wing flow -->' + 
+            '                                                <div><i class="fas fa-tint fa-fw" style="color: #868B94;"></i> <span' + 
+            '                                                        class="ms-1"> '+ response.main.humidity+'% </span>' + 
+            '                                                </div><!-- raining rate -->' + 
+            '                                                <div><i class="fas fa-arrow-alt-circle-down" style="color: #868B94;"></i> <span' + 
+            '                                                        class="ms-1"> '+response.main.pressure+' hPa </span>' + 
+            '                                                </div><!-- raining time -->' + 
+            '                                            </div>' + 
+            '                                            <div>' + 
+            '                                                <img src="http://openweathermap.org/img/wn/'+response.weather[0].icon+'@4x.png"' + 
+            '                                                    width="100px"><!-- icon pic -->' + 
+            '                                            </div>' + 
+            '                                        </div>' + 
+            '                                    </div>' + 
+            '                                </div>' + 
+            '                            </div>' + 
+            '';
+        $("#carddeck").append(card);
+        },
+        error: function (response) {
+            console.log('response: ', response);
+            return "error";
+        },
+    });
+}
+
 function main() {
-    callAPI();
+    
+    
+    $("#search").click(function () {
+        geolocation = $("#geolocation").val();
+        console.log(geolocation)
+        findLocation(geolocation);
+    })
+    $("#CurrentLocation").click(function () {
     getCurrentLocation();
+    })
+    
 }
 $(document).ready(main);
--- server.js
+++ server.js
@@ -1,17 +1,84 @@
-var express = require("express"),
-    
-    app;
-// Create our Express-powered HTTP server
-// and have it listen or port 3000
-app = express();
+var express = require("express")
+var app = express();
+var bcrypt = require('bcrypt')
+var passport = require('passport')
+var flash = require('express-flash')
+var session = require('express-session')
+var methodOverride = require('method-override')
+
+const initializePassport = require('/passport-config')
+initializePassport(
+    passport, 
+    username => users.find(user => user.username === username),
+    id => users.find(user => user.id === id)
+)
+
+const users = [] // < ------------------------------------------------------------------- change to database
+
+app.set('view-engine', 'html')
+app.use(express.urlencoded({extended: false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get("/", checkAuthenticated,(req, res) => {
+    res.render("main.html", { name: req.user.fname});  
+});
+
+app.get("/", checkNotAuthenticated,(req, res) => {
+    res.render("login.html");  
+});
+
+app.post("/login", checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+app.get("/signup", checkNotAuthenticated, (req, res) => {
+    res.render("signup.html");  
+});
+
+app.post("/signup", checkNotAuthenticated, async (req, res) => {
+    try{
+        var hashedPassword = bcrypt.hash(req.body.password, 10)
+        users.push({ //also this one
+            id: Date.now().toString(),
+            fname: req.body.fname,
+            lname: req.body.lname,
+            email: req.body.email,
+            username: req.body.username,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    }catch{
+        res.redirect('/signup')
+    }
+})
+
+app.delete('/logout', (req,res) => {
+    req.logout()
+    res.redirect('/login')
+}) 
+
+function checkAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(res.isAuthenticated()){
+        return res.redirect('/')
+    }
+    next()
+}
+
 app.listen(3000)
-// set up our routes
-
-
-app.get("/hello", function (req, res) {
-    res.send("Hello World!");
-    
-});
-app.get("/goodbye", function (req, res) {
-    res.send("Goodbye World!");
-});
